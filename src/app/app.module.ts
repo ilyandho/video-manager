6380d15ec53c467b8cc67123ef267879cf883035
Ilyas Ndhote <ii.ndhote@gmail.com>
2019-11-12 22:01:25 +0300

    First commit with navbar and routers set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { ExploreComponent } from "./components/explore/explore.component";
+import { UploadComponent } from "./components/upload/upload.component";
+
+const appRoutes: Routes = [
+  { path: "home", component: UploadComponent },
+  { path: "upload", component: UploadComponent },
+  { path: "explore", component: ExploreComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ExploreComponent,
+    UploadComponent,PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
